336765244426f687d493417b40bc9f16c7f84bf9
Will Godfrey <666713+willgodfrey@users.noreply.github.com>
2023-05-26 21:11:14 -0600

    First commit

--- main.tf
+++ main.tf
@@ -4,6 +4,126 @@ terraform {
       source = "hashicorp/google"
       version = "4.66.0"
     }
+     kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.20.0"
+    }
+    googleworkspace = {
+      source = "hashicorp/googleworkspace"
+      version = "0.7.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.9.0"
+    }
+    salesforce = {
+      source = "hashicorp/salesforce"
+      version = "0.1.0"
+    }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "4.6.0"
+    }
+    github = {
+      source = "integrations/github"
+      version = "5.25.1"
+    }
+    grafana = {
+      source = "grafana/grafana"
+      version = "1.40.1"
+    }
+    eroku = {
+      source = "heroku/heroku"
+      version = "5.2.2"
+    }
+    inode = {
+      source = "linode/linode"
+      version = "2.3.0"
+    }
+    onepassword = {
+      source = "1Password/onepassword"
+      version = "1.1.4"
+    }
+    ansible = {
+      source = "ansible/ansible"
+      version = "1.1.0"
+    }
+    anypoint = {
+      source = "mulesoft-anypoint/anypoint"
+      version = "1.5.2"
+    }
+    pihole = {
+      source = "ryanwholey/pihole"
+      version = "0.0.12"
+    }
+    prowlarr = {
+      source = "devopsarr/prowlarr"
+      version = "1.5.0"
+    }
+    radarr = {
+      source = "devopsarr/radarr"
+      version = "1.8.0"
+    }
+    readarr = {
+      source = "devopsarr/readarr"
+      version = "2.0.0"
+    }
+    sonarr = {
+      source = "devopsarr/sonarr"
+      version = "2.8.0"
+    }
+    whisparr = {
+      source = "devopsarr/whisparr"
+      version = "1.2.0"
+    }
+    twilio = {
+      source = "twilio/twilio"
+      version = "0.18.22"
+    }
+    flux = {
+      source = "fluxcd/flux"
+      version = "1.0.0-rc.3"
+    }
+    gitea = {
+      source = "go-gitea/gitea"
+      version = "0.1.0"
+    }
+    synology = {
+      source = "arnouthoebreckx/synology"
+      version = "0.2.0"
+    }
+    turing-pi-bmc = {
+      source = "jlec/turing-pi-bmc"
+      version = "0.0.4"
+    }
+    routeros = {
+      source = "terraform-routeros/routeros"
+      version = "1.10.2"
+    }
+    unifi = {
+      source = "paultyng/unifi"
+      version = "0.41.0"
+    }
+    slack-app = {
+      source = "change-engine/slack-app"
+      version = "0.1.0"
+    }
+    slack-token = {
+      source = "change-engine/slack-token"
+      version = "0.1.3"
+    }
+    tailscale = {
+      source = "tailscale/tailscale"
+      version = "0.13.7"
+    }
+    dns = {
+      source = "hashicorp/dns"
+      version = "3.3.2"
+    }
+    whatsmyip = {
+      source = "dewhurstwill/whatsmyip"
+      version = "1.0.3"
+    }
   }
 }
 
@@ -11,403 +131,120 @@ provider "google" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.20.0"
-    }
-  }
-}
-
 provider "kubernetes" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    googleworkspace = {
-      source = "hashicorp/googleworkspace"
-      version = "0.7.0"
-    }
-  }
 }
 
 provider "googleworkspace" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    helm = {
-      source = "hashicorp/helm"
-      version = "2.9.0"
-    }
-  }
-}
-
 provider "helm" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    salesforce = {
-      source = "hashicorp/salesforce"
-      version = "0.1.0"
-    }
-  }
 }
 
 provider "salesforce" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    ably = {
-      source = "ably/ably"
-      version = "0.4.3"
-    }
-  }
-}
-
-provider "ably" {
-  # Configuration options
-}
-
-terraform {
-  required_providers {
-    cloudflare = {
-      source = "cloudflare/cloudflare"
-      version = "4.6.0"
-    }
-  }
-}
-
 provider "cloudflare" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    github = {
-      source = "integrations/github"
-      version = "5.25.1"
-    }
-  }
 }
 
 provider "github" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    grafana = {
-      source = "grafana/grafana"
-      version = "1.40.1"
-    }
-  }
-}
-
 provider "grafana" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    heroku = {
-      source = "heroku/heroku"
-      version = "5.2.2"
-    }
-  }
 }
 
 provider "heroku" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    linode = {
-      source = "linode/linode"
-      version = "2.3.0"
-    }
-  }
-}
-
 provider "linode" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    onepassword = {
-      source = "1Password/onepassword"
-      version = "1.1.4"
-    }
-  }
 }
 
 provider "onepassword" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    ansible = {
-      source = "ansible/ansible"
-      version = "1.1.0"
-    }
-  }
-}
-
 provider "ansible" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    anypoint = {
-      source = "mulesoft-anypoint/anypoint"
-      version = "1.5.2"
-    }
-  }
 }
 
 provider "anypoint" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    pihole = {
-      source = "ryanwholey/pihole"
-      version = "0.0.12"
-    }
-  }
-}
-
 provider "pihole" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    prowlarr = {
-      source = "devopsarr/prowlarr"
-      version = "1.5.0"
-    }
-  }
 }
 
 provider "prowlarr" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    radarr = {
-      source = "devopsarr/radarr"
-      version = "1.8.0"
-    }
-  }
-}
-
 provider "radarr" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    readarr = {
-      source = "devopsarr/readarr"
-      version = "2.0.0"
-    }
-  }
 }
 
 provider "readarr" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    sonarr = {
-      source = "devopsarr/sonarr"
-      version = "2.8.0"
-    }
-  }
-}
-
 provider "sonarr" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    whisparr = {
-      source = "devopsarr/whisparr"
-      version = "1.2.0"
-    }
-  }
 }
 
 provider "whisparr" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    twilio = {
-      source = "twilio/twilio"
-      version = "0.18.22"
-    }
-  }
-}
-
 provider "twilio" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    flux = {
-      source = "fluxcd/flux"
-      version = "1.0.0-rc.3"
-    }
-  }
 }
 
 provider "flux" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    gitea = {
-      source = "go-gitea/gitea"
-      version = "0.1.0"
-    }
-  }
-}
-
 provider "gitea" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    synology = {
-      source = "arnouthoebreckx/synology"
-      version = "0.2.0"
-    }
-  }
 }
 
 provider "synology" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    turing-pi-bmc = {
-      source = "jlec/turing-pi-bmc"
-      version = "0.0.4"
-    }
-  }
-}
-
 provider "turing-pi-bmc" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    routeros = {
-      source = "terraform-routeros/routeros"
-      version = "1.10.2"
-    }
-  }
 }
 
 provider "routeros" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    unifi = {
-      source = "paultyng/unifi"
-      version = "0.41.0"
-    }
-  }
-}
-
 provider "unifi" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    slack-app = {
-      source = "change-engine/slack-app"
-      version = "0.1.0"
-    }
-  }
 }
 
 provider "slack-app" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    slack-token = {
-      source = "change-engine/slack-token"
-      version = "0.1.3"
-    }
-  }
-}
-
 provider "slack-token" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    tailscale = {
-      source = "tailscale/tailscale"
-      version = "0.13.7"
-    }
-  }
 }
 
 provider "tailscale" {
   # Configuration options
 }
 
-terraform {
-  required_providers {
-    dns = {
-      source = "hashicorp/dns"
-      version = "3.3.2"
-    }
-  }
-}
-
 provider "dns" {
   # Configuration options
-}
-
-terraform {
-  required_providers {
-    whatsmyip = {
-      source = "dewhurstwill/whatsmyip"
-      version = "1.0.3"
-    }
-  }
 }
 
 provider "whatsmyip" {
